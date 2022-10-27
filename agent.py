2293265e54b1513075cb3070c2497af6e6404c83
Jorrit Jan Walinga <j.j.walinga@students.uu.nl>
2022-10-27 03:00:12 +0200

    (hopefully) made natural language more interpretable for the agent

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from owlready2 import *
 from pprint import pprint
+from nltk.corpus import WordNet
 
 class Agent:
     
@@ -14,4 +15,29 @@ class Agent:
         pprint(self.onto.individuals)
     
     def query(self, query):
-        return self.onto.search(query)
+        #the query is transformed into a list of words
+        whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        transformedInput = ''.join(filter(whitelist.__contains__, query))
+        sentenceListUncap = transformedInput1.split()
+        sentenceList = map(lambda word: word.capitalize(), sentenceListUncap)
+
+        #looping over the list and mapping words to something in the ontology if possible
+        interpretableList = []
+        for i in sentenceList:
+            searchWord = '*' + i
+            ontList = onto.search(iri = searchWord)
+            if ontList:
+                interpretableList.append(ontList[0])
+            else:
+                synonyms = []
+                for syn in wordnet.synsets(i.lower()):
+                    for j in syn.lemmas():
+                     synonyms.append(j.name())
+                for synonym in synonyms:
+                    searchSynonym = '*' + synonym.capitalize()
+                    synList = onto.search(iri = searchSynonym)
+                    if synList:
+                        interpretableList.append(synList[0])
+                        break
+
+        return interpretableList
